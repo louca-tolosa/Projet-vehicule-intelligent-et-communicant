3c06c73d4afc8197b0d98fcfe2dc4ba0d9044f39
KellyLuo-O <kkelly.luo@gmail.com>
2023-03-21 15:49:03 +0100

    Ajout des librairies et configuration des broches spi

--- 2021_STM32F4_base_etudiants/RTE/Device/STM32F407VGTx/RTE_Device.h
+++ 2021_STM32F4_base_etudiants/RTE/Device/STM32F407VGTx/RTE_Device.h
@@ -1222,10 +1222,10 @@
 
 // <e> SPI1 (Serial Peripheral Interface 1) [Driver_SPI1]
 // <i> Configuration settings for Driver_SPI1 in component ::CMSIS Driver:SPI
-#define RTE_SPI1                        0
+#define RTE_SPI1                        1
 
 //   <o> SPI1_MISO Pin <0=>Not Used <1=>PA6 <2=>PB4
-#define RTE_SPI1_MISO_PORT_ID           0
+#define RTE_SPI1_MISO_PORT_ID           1
 #if    (RTE_SPI1_MISO_PORT_ID == 0)
 #define RTE_SPI1_MISO                   0
 #elif  (RTE_SPI1_MISO_PORT_ID == 1)
@@ -1241,7 +1241,7 @@
 #endif
 
 //   <o> SPI1_MOSI Pin <0=>Not Used <1=>PA7 <2=>PB5
-#define RTE_SPI1_MOSI_PORT_ID           0
+#define RTE_SPI1_MOSI_PORT_ID           1
 #if    (RTE_SPI1_MOSI_PORT_ID == 0)
 #define RTE_SPI1_MOSI                   0
 #elif  (RTE_SPI1_MOSI_PORT_ID == 1)
@@ -1269,7 +1269,7 @@
 #endif
 
 //   <o> SPI1_NSS Pin <0=>Not Used <1=>PA4 <2=>PA15
-#define RTE_SPI1_NSS_PORT_ID            0
+#define RTE_SPI1_NSS_PORT_ID            1
 #if    (RTE_SPI1_NSS_PORT_ID == 0)
 #define RTE_SPI1_NSS_PIN                0
 #elif  (RTE_SPI1_NSS_PORT_ID == 1)
--- 2021_STM32F4_base_etudiants/RTE/RTE_Components.h
+++ 2021_STM32F4_base_etudiants/RTE/RTE_Components.h
@@ -20,6 +20,13 @@
 #define RTE_DEVICE_HAL_GPIO
 #define RTE_DEVICE_HAL_PWR
 #define RTE_DEVICE_HAL_RCC
+#define RTE_DEVICE_HAL_SPI
 #define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+#define RTE_Drivers_SPI1                /* Driver SPI1 */
+        #define RTE_Drivers_SPI2                /* Driver SPI2 */
+        #define RTE_Drivers_SPI3                /* Driver SPI3 */
+        #define RTE_Drivers_SPI4                /* Driver SPI4 */
+        #define RTE_Drivers_SPI5                /* Driver SPI5 */
+        #define RTE_Drivers_SPI6                /* Driver SPI6 */
 
 #endif /* RTE_COMPONENTS_H */
