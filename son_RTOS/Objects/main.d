fc18de9e818336ea9ceba075edaea053dacc671f
kamilk93 <129323127+kamilk93@users.noreply.github.com>
2023-03-30 09:46:11 +0200

    Add files via upload

---
+++ son_RTOS/Objects/main.d
@@ -0,0 +1,8 @@
+.\objects\main.o: main.c
+.\objects\main.o: osObjects.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\RTOS\RTX\INC\cmsis_os.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Driver\Include\Driver_USART.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Driver\Include\Driver_Common.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
